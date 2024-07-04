0b06a6f21213f24d62c8f66026159e91092aa69d
Emile0001 <boteseh@gmail.com>
2024-07-04 15:38:00 +0200

    object

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ console.log(getWage(baseSalary, overtime, rate));
 
 //OOP
 
+//Object literal
+
 let employee = {
     baseSalary2: 40000,
     overtime2: 20,
@@ -38,3 +40,16 @@ function calcSum(num1, num2) {
 const result = calcSum(1, 2);
 console.log(result); // Logs the object
 console.log(result.total()); // Logs the sum, which is 3
+
+//Constructor Function
+
+function Calculator(num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+    this.total = function () {
+        return this.num1 + this.num2;
+    };
+}
+const calc = new Calculator(1, 2);
+
+console.log(calc.total());
