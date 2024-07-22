2b5b73d83fb6e8ecd6a6de51e451fd7772eb5e75
Emile0001 <boteseh@gmail.com>
2024-07-22 11:55:57 +0200

    stopwatch

--- stopwatch.js
+++ stopwatch.js
@@ -1,9 +1,46 @@
-//sw.start
-//sw.stop
-//sw.duration
-
 function Stopwatch() {
-    this.start = start;
-    this.stop = stop;
-    this.duration = duration;
+    let startTime = 0,
+        endTime = 0,
+        running = false,
+        duration = 0;
+
+    this.start = function () {
+        if (!running) {
+            running = true;
+            startTime = performance.now();
+        } else throw new Error("Stopwatch has already started.");
+    };
+
+    this.stop = function () {
+        if (running) {
+            running = false;
+            endTime = performance.now();
+            duration += endTime - startTime;
+        } else throw new Error("Stopwatch has not started.");
+    };
+
+    this.reset = function () {
+        startTime = 0;
+        endTime = 0;
+        running = false;
+        duration = 0;
+    };
+
+    this.duration = function () {
+        if (running) {
+            return performance.now() - startTime + duration;
+        } else {
+            return duration;
+        }
+    };
 }
+
+// Usage:
+const stopwatch = new Stopwatch();
+stopwatch.start();
+
+// Simulate a delay
+setTimeout(() => {
+    stopwatch.stop();
+    console.log("Elapsed time (ms):", stopwatch.duration());
+}, 1000);
